1f17069ad436c33ac2b0d7437e99e15c4d59b610
lilia570 <liliapaslavska570@gmail.com>
2025-10-31 20:01:11 +0200

    correct

--- src/task-8.ts
+++ src/task-8.ts
@@ -5,7 +5,7 @@ interface Post {
 }
 import axios from "axios";
 
-async function fetchPosts() {
+async function fetchPosts(): Promise<Post[]> {
   const response = await axios.get<Post[]>(
     'https://jsonplaceholder.typicode.com/posts'
   );
